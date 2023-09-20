312e224473f48fae6131a3a5b5c74e916f9dc687
rpm <ryan.macarthur@gmail.com>
2023-09-19 20:43:53 -0400

    init

---
+++ src/main.rs
@@ -0,0 +1,42 @@
+//! Implements a hello-world example for Arbitrum Stylus, providing a Solidity ABI-equivalent
+//! Rust implementation of the Counter contract example provided by Foundry.
+//! Warning: this code is a template only and has not been audited.
+//! ```
+//! contract Counter {
+//!     uint256 public number;
+//!     function setNumber(uint256 newNumber) public {
+//!         number = newNumber;
+//!     }
+//!     function increment() public {
+//!         number++;
+//!     }
+//! }
+//! ```
+
+// Only run this as a WASM if the export-abi feature is not set.
+#![cfg_attr(not(feature = "export-abi"), no_main)]
+extern crate alloc;
+mod verify;
+/// Initializes a custom, global allocator for Rust programs compiled to WASM.
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+/// Import the Stylus SDK along with alloy primitive types for use in our program.
+use stylus_sdk::{alloy_primitives::U256, prelude::*};
+
+// Define the entrypoint as a Solidity storage object, in this case a struct
+// called `Counter` with a single uint256 value called `number`. The sol_storage! macro
+// will generate Rust-equivalent structs with all fields mapped to Solidity-equivalent
+// storage slots and types.
+#[solidity_storage]
+#[entrypoint]
+pub struct TLSNVerifier;
+
+/// Define an implementation of the generated Counter struct, defining a set_number
+#[external]
+impl TLSNVerifier {
+    pub fn tlsnverify()-> Result<(), Vec<u8>> {
+        verify::verify();
+        Ok(())
+    }
+}
